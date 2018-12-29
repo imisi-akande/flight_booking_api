15807f529d38edba43d0d7019e8d77a8714568cd
Imisioluwa Akande <imisioluwa.akande@andela.com>
2018-12-29 13:26:51 +0100

    setup configuration for travis

--- flightapi/flightapi/test.py
+++ flightapi/flightapi/test.py
@@ -4,14 +4,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-SECRET_KEY = ')uya@is*$8!1luag=_v1jnog0yo)7*&c%$sn_x3qn#ysg6uzt6'
 ## Database config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travisdb',
-        'USER': 'AkandeImisioluwa',
-        'HOST': '127.0.0.1',
-        'PORT': ''
+        'USER': 'postgres',
+        'HOST': '127.0.0.1'
     }
 }
