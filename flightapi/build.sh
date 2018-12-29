b34150f77b5c9583e331411934986e5c62d4318a
Imisioluwa Akande <imisioluwa.akande@andela.com>
2018-12-29 15:30:56 +0100

    fix absolute imports

--- flightapi/build.sh
+++ flightapi/build.sh
@@ -1,5 +1,5 @@
 # stops execution if there is an error
-set -e 
+set -e
 python manage.py makemigrations
 python manage.py migrate
 coverage run --source='.' manage.py test
---
+++ flightapi/flightapi/settings/test.py
@@ -0,0 +1,15 @@
+from flightapi.settings.base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+## Database config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travisdb',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1'
+    }
+}
