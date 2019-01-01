5e01cc692004f1059c97dc344499aacec334428f
Imisioluwa Akande <imisioluwa.akande@andela.com>
2019-01-01 23:42:48 +0100

    configure django heroku

--- flightapi/flightapi/settings/base.py
+++ flightapi/flightapi/settings/base.py
@@ -190,3 +190,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, *MEDIA_URL.strip("/").split("/"))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
